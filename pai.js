6447cc2be059bff89f3aee4777f7772dcef4e96f
huangxl <huangxl@sdc.icbc.com.cn>
2015-09-06 18:10:39 +0800

    on事件覆盖，改成attachEvent的bind方法。修改mousemove事件的监听间隔，确保和click的顺序。

--- pai.js
+++ pai.js
@@ -42,38 +42,44 @@ var _pai = function(sid) {
 		var getEleId = function(ele) {
 			return ele && (ele.id || ele.name || ele.tagName);
 		};
-		// TODO: on事件覆盖，改成attachEvent的bind方法
-		document.body.onclick = function() {
-			p.push({"e" : "onclick", "x" : event.clientX, "y" : event.clientY, "srcElement" : getEleId(event.srcElement)});
-		};
-		document.body.onkeyup = function() {
-			p.push({"e" : "onkeyup", "keyCode" : event.keyCode, "ctrlKey" : event.ctrlKey, "altKey" : event.altKey, "shiftKey" : event.shiftKey, "srcElement" : getEleId(event.srcElement)});
-		};
+		var eventInject = function(obj, eventname, func) {
+			if (obj.addEventListener) {
+				obj.addEventListener(eventname, func, false);
+			} else if (obj.attachEvent) {
+				obj.attachEvent("on" + eventname, func);
+			}
+		}
+		eventInject(document.body, 'click', function() {
+			p.push({"e" : "click", "x" : event.clientX, "y" : event.clientY, "srcElement" : getEleId(event.srcElement)});
+		});
+		eventInject(document.body, 'keyup', function() {
+			p.push({"e" : "keyup", "keyCode" : event.keyCode, "ctrlKey" : event.ctrlKey, "altKey" : event.altKey, "shiftKey" : event.shiftKey, "srcElement" : getEleId(event.srcElement)});
+		});
 		_pai.moveTimer = null;
 		_pai.oldX = null;
 		_pai.oldY = null;
-		document.body.onmousemove = function() {
+		eventInject(document.body, 'mousemove', function() {
 			_pai.moveTimer && clearTimeout(_pai.moveTimer);
 			var cx = event.clientX;
 			var cy = event.clientY;
 			_pai.moveTimer = setTimeout(function() {
 				if (_pai.oldX != cx || _pai.oldY != cy) {
-					p.push({"e" : "onmousemove", "x" : cx, "y" : cy, "srcElement" : getEleId(document.elementFromPoint(cx, cy))}, -200);
+					// TODO: 由250改成100是因为mousemove与click的反序问题，因为mouseevent是等待250不动之后才触发的，因此点击会先有，然后才有mousemove，改成100才能符合人类移动+点击的速度
+					p.push({"e" : "mousemove", "x" : cx, "y" : cy, "srcElement" : getEleId(document.elementFromPoint(cx, cy))}, -100);
 					_pai.oldX = cx;
 					_pai.oldY = cy;
 				}
-			}, 250);
-		};
+			}, 100);
+		});
 		_pai.scrollTimer = null;
-		window.onscroll = function() {
+		eventInject(window, 'scroll', function() {
 			_pai.scrollTimer && clearTimeout(_pai.scrollTimer);
 			_pai.scrollTimer = setTimeout(function() {
-				p.push({"e" : "onscroll", "scrollTop" : document.documentElement.scrollTop || document.body.scrollTop});
+				p.push({"e" : "scroll", "scrollTop" : document.documentElement.scrollTop || document.body.scrollTop});
 			}, 250);
-		};
+		});
 		var getViewPortSize = function() {
 			// TODO: 这段代码还得继续调一下，不太准，比如Chrome上是算滚动条的，但是IE是不算的
-			// TODO: 判断分支要不要缓存提速？有点影响resize时候的动态
 			// Standards compliant browsers (chrome/mozilla/netscape/opera)
 			if (typeof window.innerWidth != 'undefined') {
 				return [window.innerWidth, window.innerHeight];
@@ -86,18 +92,16 @@ var _pai = function(sid) {
 			}
 		};
 		_pai.resizeTimer = null;
-		window.onresize = function() {
+		eventInject(window, 'resize', function() {
 			// TODO: 级联resize会被触发么？需不需要只挂在top上？但这样内部的可调大小的事件是不是就没有了？
 			_pai.resizeTimer && clearTimeout(_pai.resizeTimer);
 			_pai.resizeTimer = setTimeout(function() {
-				p.push({"e" : "onresize", "viewport" : getViewPortSize(), "screnn" : [screen.width, screen.height], "pos" : [window.screenLeft, window.screenTop]});
+				p.push({"e" : "resize", "viewport" : getViewPortSize(), "screnn" : [screen.width, screen.height], "pos" : [window.screenLeft, window.screenTop]});
 			}, 250);
-		}
+		});
 		// TODO: DIV、TEXTAREA等的onscroll没有截取到（不冒泡），iframe的有截取到
-		// TODO: onunload
 		// TODO: onfocus（不冒泡）如果记录了mousemove和keyuptab，是不是可以认为就是可以区分focus了
 		// TODO: onbeforeunload
 	};
 	window.onload = p.domready;
 };
-
