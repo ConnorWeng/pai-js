ff9c63ef85500c2402f592249f8591dc6cac1553
huangxl <huangxl@sdc.icbc.com.cn>
2015-09-06 15:04:41 +0800

    增加resize支持，修改各类Timer事件实现

--- pai.js
+++ pai.js
@@ -38,20 +38,22 @@ var _pai = function(sid) {
 		console.log(window.localStorage.getItem("_pai"));
 		window.localStorage.removeItem("_pai");
 	}
-	var getEleId = function(ele) {
-		return ele && (ele.id || ele.name || ele.tagName);
-	};
 	p.domready = function() {
+		var getEleId = function(ele) {
+			return ele && (ele.id || ele.name || ele.tagName);
+		};
+		// TODO: on事件覆盖，改成attachEvent的bind方法
 		document.body.onclick = function() {
 			p.push({"e" : "onclick", "x" : event.clientX, "y" : event.clientY, "srcElement" : getEleId(event.srcElement)})
 		};
 		document.body.onkeyup = function() {
 			p.push({"e" : "onkeyup", "keyCode" : event.keyCode, "ctrlKey" : event.ctrlKey, "altKey" : event.altKey, "shiftKey" : event.shiftKey, "srcElement" : getEleId(event.srcElement)});
 		};
+		_pai.moveTimer = null;
+		_pai.oldX = null;
+		_pai.oldY = null;
 		document.body.onmousemove = function() {
-			if (_pai.moveTimer) {
-				clearTimeout(_pai.moveTimer);
-			}
+			_pai.moveTimer && clearTimeout(_pai.moveTimer);
 			var cx = event.clientX;
 			var cy = event.clientY;
 			_pai.moveTimer = setTimeout(function() {
@@ -62,14 +64,35 @@ var _pai = function(sid) {
 				}
 			}, 250);
 		};
+		_pai.scrollTimer = null;
 		window.onscroll = function() {
-			if (_pai.scrollTimer) {
-				clearTimeout(_pai.scrollTimer)
-			}
+			_pai.scrollTimer && clearTimeout(_pai.scrollTimer);
 			_pai.scrollTimer = setTimeout(function() {
 				p.push({"e" : "onscroll", "scrollTop" : document.documentElement.scrollTop || document.body.scrollTop});
 			}, 250);
 		};
+		var getViewPortSize = function() {
+			// TODO: 这段代码还得继续调一下，不太准，比如Chrome上是算滚动条的，但是IE是不算的
+			// TODO: 判断分支要不要缓存提速？有点影响resize时候的动态
+			// Standards compliant browsers (chrome/mozilla/netscape/opera)
+			if (typeof window.innerWidth != 'undefined') {
+				return [window.innerWidth, window.innerHeight];
+			// IE6/IE8
+			} else if (document.documentElement && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) {
+				return [document.documentElement.clientWidth, document.documentElement.clientHeight];
+			// Older IE
+			} else {
+				return [document.getElementsByTagName('body')[0].clientWidth, document.getElementsByTagName('body')[0].clientHeight];
+			}
+		};
+		_pai.resizeTimer = null;
+		window.onresize = function() {
+			// TODO: 级联resize会被触发么？需不需要只挂在top上？但这样内部的可调大小的事件是不是就没有了？
+			_pai.resizeTimer && clearTimeout(_pai.resizeTimer);
+			_pai.resizeTimer = setTimeout(function() {
+				p.push({"e" : "onresize", "viewport" : getViewPortSize(), "screnn" : [screen.width, screen.height], "pos" : [window.screenLeft, window.screenTop]});
+			}, 250);
+		}
 		// TODO: DIV、TEXTAREA等的onscroll没有截取到（不冒泡），iframe的有截取到
 		// TODO: onunload
 		// TODO: onfocus（不冒泡）如果记录了mousemove和keyuptab，是不是可以认为就是可以区分focus了
@@ -78,7 +101,4 @@ var _pai = function(sid) {
 	};
 	window.onload = p.domready;
 };
-_pai.scrollTimer = null;
-_pai.moveTimer = null;
-_pai.oldX = null;
-_pai.oldY = null;
+
