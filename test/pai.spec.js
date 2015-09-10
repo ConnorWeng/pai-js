3faee9157f358a3b8bb5c696b07da0cb22916050
Connor Weng <foamtea30@gmail.com>
2015-09-10 09:08:00 +0800

    add test

--- test/pai.spec.js
+++ test/pai.spec.js
@@ -1,4 +1,16 @@
-chai.should();
+var should = chai.should();
 
 describe('pai.js', function() {
+    var pai;
+    beforeEach(function() {
+        pai = new _pai('ASDFGHJKL');
+    });
+
+    describe('saveremote', function() {
+        it('should remove items from local storage', function() {
+            window.localStorage.setItem('_pai', '{"key": "value"}');
+            pai.saveremote();
+            should.not.exist(window.localStorage.getItem('_pai'));
+        });
+    });
 });
