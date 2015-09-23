0dc06d0cd32fba4121ceef259cacc3cf14286955
Connor Weng <foamtea30@gmail.com>
2015-09-23 14:05:09 +0800

    add simple server

---
+++ src/server.js
@@ -0,0 +1,52 @@
+var http = require('http');
+var fs = require('fs');
+var parseUrl = require('url').parse;
+var resolvePath = require('path').resolve;
+
+var STATIC_RESOURCE_EXTS = ['js', 'css', 'html', 'htm', 'jpge', 'jpg', 'png', 'ico'];
+var RESOURCE_TYPE_MAP = {
+	'js': 'text/javascript',
+	'css': 'text/stylesheet',
+	'html': 'text/html',
+	'htm': 'text/html',
+	'jpeg': 'image/jpeg',
+	'jpg': 'image/jpeg',
+	'png': 'image/png',
+	'ico': 'image/ico'
+};
+var HANDLERS = [handleStaticResource, handleMessage];
+
+http.createServer(function(req, res) {
+	var next = function(err, statusCode) {
+		if (err) handleError(req, res, statusCode, err);
+	};
+	var i = 0;
+	for(; i < HANDLERS.length; i++) {
+		var handler = HANDLERS[i];
+		if (handler(req, res, next)) break;
+	}
+	if (i === HANDLERS.length) {
+		handleError(req, res, 500, new Error('no handler for this request'));
+	}
+}).listen(process.env.PAI_PORT || 9999);
+
+function handleStaticResource(req, res, next) {
+	var path = parseUrl(req.url).path.substr(1);
+	var ext = path.substr(path.lastIndexOf('.') + 1);
+	if (req.method !== 'GET' || !~STATIC_RESOURCE_EXTS.indexOf(ext)) return false;
+	fs.readFile(resolvePath(path), function(err, data) {
+		if (err) next(new Error('request resouce not exists'), 404);
+		res.writeHead(200, {'Content-Type': RESOURCE_TYPE_MAP[ext]});
+		res.end(data);
+	});
+	return true;
+}
+
+function handleMessage(req, res, next) {
+	return false;
+}
+
+function handleError(req, res, statusCode, err) {
+	res.writeHead(statusCode, {'Content-Type': 'text/plain'});
+	res.end(err.message);
+}
