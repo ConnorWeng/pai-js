e87d3f22977b777b4a9cdfdadb9573e54954daba
Connor Weng <foamtea30@gmail.com>
2015-12-22 14:56:51 +0800

    fix bugs caused by frameset

--- src/frontend/pai.js
+++ src/frontend/pai.js
@@ -192,8 +192,12 @@ var _pai = function(sid) {
 			} else if (document.documentElement && typeof document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth !== 0) {
 				return [document.documentElement.clientWidth, document.documentElement.clientHeight];
 			// Older IE
-			} else {
+			} else if (document.getElementsByTagName('body').length > 0) {
 				return [document.getElementsByTagName('body')[0].clientWidth, document.getElementsByTagName('body')[0].clientHeight];
+			} else if (document.getElementsByTagName('frameset').length > 0) {
+				return [document.getElementsByTagName('frameset')[0].clientWidth, document.getElementsByTagName('frameset')[0].clientHeight];
+			} else {
+				return '';
 			}
 		};
 		_pai.resizeTimer = null;
